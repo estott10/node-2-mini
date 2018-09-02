ad0e5bb6b90d081ac89f8c4c23e5e431aee3274f
ErikaStott <erika.r.stott@gmail.com>
2018-09-01 18:24:55 -0700

    completed

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const cors = require('cors');
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
+const massive= require('massive');
+const controller= require('./controller')
+
+
+require('dotenv').config()
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance);
+    // dbInstance.get_planes()
+    //     .then( planes => console.log( planes ) )
+    //     .catch( err => console.log( err ) );
+    });
+
+app.get('/api/planes', controller.getPlanes);
 
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
